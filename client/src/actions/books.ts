3c31d2773bcf3f8aaa464bc61ca0a9ebe817b76f
Ilia <verzunilia@gmail.com>
2019-06-25 11:11:51 +0300

    new commit

--- client/src/actions/books.ts
+++ client/src/actions/books.ts
@@ -2,14 +2,14 @@
 import axios from 'axios';
 import { environment } from '../enviroments/enviroments';
 
-export function booksFetchDataSuccess(books: any) {
+export function booksFetchDataSuccess(books: any): Object {
     return {
         type: "BOOKS_FETCH_DATA_SUCCESS",
         books
     }
 }
 
-export function booksFetchData(url: any) {
+export function booksFetchData(url: any): Object {
     return (dispatch: any) => {
         axios.get(`${environment.mySql.databaseURL}/booksAuthors/getAllbooksAuthors`)
         .then((res: any) => {
